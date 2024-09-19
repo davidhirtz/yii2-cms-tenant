2db09bb7ba398fa5e19f3cef4223f0144973b361
davidhirtz <david.hirtz@me.com>
2024-09-19 21:32:52 +0200

    Fixed `baseSlug`

--- src/assets/src/dropdown.ts
+++ src/assets/src/dropdown.ts
@@ -5,17 +5,14 @@ export default (
     const $tenantIdDropdown: HTMLSelectElement = document.querySelector(tenantDropdownSelector);
     const $parentDropdown: HTMLSelectElement = document.querySelector(parentDropdownSelector);
 
-    let currentValue = $tenantIdDropdown.value;
+    const updateDefaultParentDropdownValue = () => {
+        $parentDropdown.options[0].dataset.value = $tenantIdDropdown.options[$tenantIdDropdown.selectedIndex].dataset.value;
+        $parentDropdown.dispatchEvent(new Event('change'));
+    }
 
     $tenantIdDropdown.addEventListener('change', function () {
-        if($tenantIdDropdown.value === currentValue) {
-            return;
-        }
-
         const url = new URL(window.location.href);
-
-        currentValue = $tenantIdDropdown.value;
-        url.searchParams.set('tenant', currentValue);
+        url.searchParams.set('tenant', $tenantIdDropdown.value);
 
         $parentDropdown.disabled = true;
 
@@ -27,15 +24,11 @@ export default (
                 const $newDropdown = doc.querySelector(parentDropdownSelector);
 
                 $parentDropdown.innerHTML = $newDropdown.innerHTML || '';
-
-                // Override the default value with the correct tenant absolute URL.
-                $parentDropdown.options[0].dataset.value = $tenantIdDropdown.options[$tenantIdDropdown.selectedIndex].dataset.value;
-
                 $parentDropdown.disabled = false;
-                $parentDropdown.dispatchEvent(new Event('change'));
+
+                updateDefaultParentDropdownValue();
             });
     });
 
-    // Trigger the tenant dropdown change event once to apply the initial parent dropdown values.
-    $tenantIdDropdown.dispatchEvent(new Event('change'));
+    updateDefaultParentDropdownValue();
 }
